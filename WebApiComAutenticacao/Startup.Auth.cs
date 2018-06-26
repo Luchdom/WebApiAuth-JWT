31885febe04ca6c1dfa7b9355babde20c436c69c
DESKTOP-S5H75IN\lucas <lucas..h.domingues@gmail.com.br>
2018-06-26 19:32:03 -0300

    Initial

---
+++ WebApiComAutenticacao/Startup.Auth.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using WebApiComAutenticacao.TokenProvider;
+
+namespace WebApiComAutenticacao
+{
+    public partial class Startup
+    {
+        public void ConfigureJwtAuthService(IServiceCollection services)
+        {
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("TokenAuthentication:SecretKey").Value));
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                // The signing key must match!
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                // Validate the JWT Issuer (iss) claim
+                ValidateIssuer = true,
+                ValidIssuer = Configuration.GetSection("TokenAuthentication:Issuer").Value,
+                // Validate the JWT Audience (aud) claim
+                ValidateAudience = true,
+                ValidAudience = Configuration.GetSection("TokenAuthentication:Audience").Value,
+                // Validate the token expiry
+                ValidateLifetime = false,
+                // If you want to allow a certain amount of clock drift, set that here:
+                ClockSkew = TimeSpan.Zero
+            };
+
+
+            services.AddAuthentication(
+                  // Seta o JWT como default de autenticação
+                  options =>
+                  {
+                      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                  }
+                )
+                .AddJwtBearer(options => {
+                    options.Audience = Configuration.GetSection("TokenAuthentication:Audience").Value;
+                    options.TokenValidationParameters = tokenValidationParameters;
+                    options.RequireHttpsMetadata = false;
+                });
+
+
+            services.BuildServiceProvider();
+            var serviceProvider = services.BuildServiceProvider();
+
+            //resolve implementations
+            //_userService = serviceProvider.GetService<IUserService>();
+        }
+
+         private void ConfigureAuth(IApplicationBuilder app)
+        {
+            //app.UseAuthentication();
+
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("TokenAuthentication:SecretKey").Value));
+            
+            var tokenProviderOptions = new TokenProviderOptions
+            {
+                Path = Configuration.GetSection("TokenAuthentication:TokenPath").Value,
+                Audience = Configuration.GetSection("TokenAuthentication:Audience").Value,
+                Issuer = Configuration.GetSection("TokenAuthentication:Issuer").Value,
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
+                IdentityResolver = GetIdentityAsync
+            };
+
+            app.UseMiddleware<TokenProviderMiddleware>(Options.Create(tokenProviderOptions));
+            
+        }
+
+        //private IUserService _userService;
+        private async Task<ClaimsIdentity> GetIdentityAsync(string username, string password)
+        {
+
+            //var result = await _userService.GetByCredentialAsync(username, password);
+            //if (result != null)
+            //{
+            //    return new ClaimsIdentity(new GenericIdentity(username, "Token"),
+            //        new Claim[] {
+            //            new Claim(ClaimTypes.NameIdentifier, result.Id.ToString()),
+            //            new Claim("name", result.Name ?? ""),
+            //    });
+            //}
+            //else
+            //    return null;
+            if (username == "TEST" && password == "TEST123")
+            {
+                return new ClaimsIdentity(new GenericIdentity(username, "Token"),
+                        new Claim[] {
+                            new Claim(ClaimTypes.NameIdentifier, username),
+                            new Claim(ClaimTypes.Name, username),
+                    });
+            }
+
+            
+            // Credentials are invalid, or account doesn't exist
+            return null;
+        }
+
+    }
+}
---
+++ WebApiComAutenticacao/TokenProvider/TokenProviderMiddleware.cs
@@ -0,0 +1,147 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebApiComAutenticacao.TokenProvider
+{
+    public class TokenProviderMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly TokenProviderOptions _options;
+        private readonly JsonSerializerSettings _serializerSettings;
+
+        public TokenProviderMiddleware(
+            RequestDelegate next,
+            IOptions<TokenProviderOptions> options)
+        {
+            _next = next;
+            
+            _options = options.Value;
+            ThrowIfInvalidOptions(_options);
+
+            _serializerSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.None,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Path.StartsWithSegments("/api"))
+            {
+                await _next.Invoke(context);
+                return;
+            }
+                if (!context.Request.Path.Equals(_options.Path, StringComparison.Ordinal))
+                {
+                    await _next.Invoke(context);
+                    return;
+                }
+
+                if (!context.Request.Method.Equals("POST")
+                        || !context.Request.HasFormContentType)
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Bad request.");
+                    return;
+                }
+
+                await GenerateToken(context);
+        }
+
+
+        private async Task GenerateToken(HttpContext context)
+        {
+            var username = context.Request.Form["username"];
+            var password = context.Request.Form["password"];
+
+            var identity = await _options.IdentityResolver(username, password);
+            if (identity == null)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Credenciais inválidas.");
+
+                return;
+            }
+
+            var now = DateTime.UtcNow;
+
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name,username),
+                new Claim(JwtRegisteredClaimNames.Sub, identity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier).Value),
+                new Claim(JwtRegisteredClaimNames.Jti, await _options.NonceGenerator()),
+                new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(now).ToUniversalTime().ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+            };
+
+            // Create the JWT and write it to a string
+            var jwt = new JwtSecurityToken(
+                issuer: _options.Issuer,
+                audience: _options.Audience,
+                claims: claims,
+                //notBefore: now,
+                //expires: now.Add(_options.Expiration),
+                signingCredentials: _options.SigningCredentials);
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                userid = identity.FindFirst(ClaimTypes.NameIdentifier).Value,
+                name = identity.FindFirst(ClaimTypes.Name).Value
+                //expires_in = (int)_options.Expiration.TotalSeconds
+            };
+
+            // Serialize and return the response
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(response, _serializerSettings));
+        }
+
+        private static void ThrowIfInvalidOptions(TokenProviderOptions options)
+        {
+            if (string.IsNullOrEmpty(options.Path))
+            {
+                throw new ArgumentNullException(nameof(TokenProviderOptions.Path));
+            }
+
+            if (string.IsNullOrEmpty(options.Issuer))
+            {
+                throw new ArgumentNullException(nameof(TokenProviderOptions.Issuer));
+            }
+
+            if (string.IsNullOrEmpty(options.Audience))
+            {
+                throw new ArgumentNullException(nameof(TokenProviderOptions.Audience));
+            }
+
+            if (options.Expiration == TimeSpan.Zero)
+            {
+                throw new ArgumentException("Must be a non-zero TimeSpan.", nameof(TokenProviderOptions.Expiration));
+            }
+
+            if (options.IdentityResolver == null)
+            {
+                throw new ArgumentNullException(nameof(TokenProviderOptions.IdentityResolver));
+            }
+
+            if (options.SigningCredentials == null)
+            {
+                throw new ArgumentNullException(nameof(TokenProviderOptions.SigningCredentials));
+            }
+
+            if (options.NonceGenerator == null)
+            {
+                throw new ArgumentNullException(nameof(TokenProviderOptions.NonceGenerator));
+            }
+        }
+    }
+}
